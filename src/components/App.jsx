7bc55c766ccb2cc7318c57ffddbc60c37ba0d7c0
PokoenkoS <pokoenkosveta@gmail.com>
2023-04-24 15:06:47 +0300

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,19 @@ import { MainDiv } from "./Main.styled";
 import Form from "./Form";
 import ContactList from "./ContactList";
 import Filter from "./Filter";
+import { useEffect } from "react";
+import { fetchContacts } from "redux/operations";
+import { useDispatch} from "react-redux";
+
+
 
 export const App = () => {
+const dispatch = useDispatch();
 
 
+useEffect(() => {
+  dispatch(fetchContacts());
+}, [dispatch]);
 
   return (
    <MainDiv>
